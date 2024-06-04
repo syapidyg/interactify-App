8a5e373d31fb4f5cb1c4c07dcc4e3314eef2226b
axcelflo <axcel.tchiffo@2025.ucac-icam.com>
2024-06-04 15:44:47 +0100

    Mes differentes pages

---
+++ Developpement/interactify_app/lib/Pages/list_chats_page.dart
@@ -0,0 +1,440 @@
+import 'package:flutter/material.dart';
+
+class ListChats extends StatelessWidget {
+  const ListChats({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // IconButton(
+            //   icon: const Icon(Icons.arrow_back_ios_new),
+            //   onPressed: () {
+            //     // Ajouter la logique de retour en arrière ici
+            //   },
+            // ),
+            const Text(
+              'Jerome Bell',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_chart),
+              onPressed: () {
+                // Ajouter la logique de prise de photo ici
+              },
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 50,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Icon(
+                    Icons.search,
+                    color: Color.fromARGB(137, 56, 55, 55),
+                  ),
+                ),
+                const Expanded(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(
+                        color: Color.fromARGB(137, 56, 55, 55),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Sent yesterday',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.more_horiz),
+                  onPressed: () {
+                    // Ajouter la logique du bouton "..."
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Developpement/interactify_app/lib/Pages/search_page.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key, required this.title});
+  final String title;
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: Icon(
+                            Icons.search,
+                            color: Color.fromARGB(137, 56, 55, 55),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: _searchController,
+                            textAlign: TextAlign.start,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                            decoration: const InputDecoration(
+                              hintText: 'Johson Carter',
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(137, 56, 55, 55),
+                                fontSize: 18,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                GestureDetector(
+                  onTap: () {
+                    // Effacer le contenu de la zone de recherche
+                    _searchController.clear();
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          // Effacer le contenu de la zone de recherche
+                          _searchController.clear();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/profil.jpeg"),
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 3),
+          Container(
+            width: double.infinity,
+            height: 2,
+            color: const Color.fromARGB(255, 190, 216, 238),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+}
